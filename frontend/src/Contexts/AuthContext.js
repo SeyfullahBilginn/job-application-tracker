4d16aef1d0094c197df5b982acf3dc79af125208
unknown <seyfullahbilgin06@gmail.com>
2022-10-13 00:12:39 +0300

    test2

--- frontend/src/Contexts/AuthContext.js
+++ frontend/src/Contexts/AuthContext.js
@@ -1,7 +1,6 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import React, { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import Alert from "@mui/material/Alert";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 
